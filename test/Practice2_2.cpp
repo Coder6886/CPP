1eac2764712ee28c2cba6c1dc58128a03dae0107
Coder6886 <64741855+Coder6886@users.noreply.github.com>
2020-05-19 20:15:22 +0800

    update of 2-2

--- test/Practice2_2.cpp
+++ test/Practice2_2.cpp
@@ -1,27 +1,20 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
 #ifdef LOCAL
-printf("Defined LOCAL");
 freopen("data2_2.in", "r", stdin);
-freopen("data2_2.out", "w", stdout);
+//freopen("data2_2.out", "w", stdout);
 #endif
-    int kase = 0, n = 1, x = 0, y = 0, z = 0;
+    int kase = 0, n = 0, x = 0, y = 0, z = 0;
+    const int c3 = 70, c5 = 21, c7 = 15;
     while (scanf("%d%d%d", &x, &y, &z) == 3){
-        n = 1;
-        for (int i = 10; i <= 100;i++){
-            if (i % 3 == x && i % 5 == y && i % 7 == z){
-                printf("Case %d: %d", ++kase, i);
-                n = 0;
-                break;
-            }
+        n = (c3*x+c5*y+c7*z)%105;
+        if(n >= 10 && n <= 100){
+            printf("Case %d: %d\n", ++kase, n);
         }
-        if (n == 1){
-            printf("Case %d: No answer", ++kase);
+        else{
+            printf("Case %d: No answer\n", ++kase);
         }
-        printf("\n");
-
     }
     return 0;
 }
